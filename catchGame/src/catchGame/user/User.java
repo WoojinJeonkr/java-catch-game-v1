a4b0f8ab2b4b4f9e06b17c78a62bb0d800759719
Jeon Woojin <96942871+WoojinJeonkr@users.noreply.github.com>
2025-04-22 16:50:16 +0900

    refactor: change constructor visibility of User to public

--- catchGame/src/catchGame/user/User.java
+++ catchGame/src/catchGame/user/User.java
@@ -6,20 +6,19 @@ public class User {
 	public String userName;
 	public String location;
 	public MonsterBase[] pokeDex;
-	
+
 	Scanner scanner = new Scanner(System.in);
-	
-	User() {
+
+	public User() {
 		System.out.print("당신의 이름은? ");
 		this.userName = scanner.nextLine();
 	}
-	
+
 	public void printPokeDex() {
-		
+
 	}
-	
+
 	public boolean insertPokeDex() {
 		return true;
 	}
 }
-
