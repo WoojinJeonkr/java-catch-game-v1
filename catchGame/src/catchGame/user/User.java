35cbb9f023c5e54cec01c753619da66062757422
Jeon Woojin <96942871+WoojinJeonkr@users.noreply.github.com>
2025-04-22 16:31:53 +0900

    feat: add User class

---
+++ catchGame/src/catchGame/user/User.java
@@ -0,0 +1,25 @@
+package catchGame.user;
+
+import java.util.Scanner;
+
+public class User {
+	public String userName;
+	public String location;
+	public MonsterBase[] pokeDex;
+	
+	Scanner scanner = new Scanner(System.in);
+	
+	User() {
+		System.out.print("당신의 이름은? ");
+		this.userName = scanner.nextLine();
+	}
+	
+	public void printPokeDex() {
+		
+	}
+	
+	public boolean insertPokeDex() {
+		return true;
+	}
+}
+
