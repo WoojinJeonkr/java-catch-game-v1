b3a651dca95648b9e62437a180d901c3752c39c3
Jeon Woojin <96942871+WoojinJeonkr@users.noreply.github.com>
2025-04-22 18:11:07 +0900

    feat: add updateMyPokeDex to User for monster capture

--- catchGame/src/catchGame/user/User.java
+++ catchGame/src/catchGame/user/User.java
@@ -3,22 +3,27 @@ package catchGame.user;
 import java.util.Scanner;
 
 public class User {
-	public String userName;
-	public String location;
-	// public MonsterBase[] pokeDex;
+	public String userName; // 사용자 이름
+	public String location; // 사용자 위치
+	public PokeDex pokeDex; // 사용자 도감
 
 	Scanner scanner = new Scanner(System.in);
 
+	// User 객체 생성 시 이름 입력받고, 새로운 도감 불러옴
 	public User() {
 		System.out.print("당신의 이름은? ");
 		this.userName = scanner.nextLine();
+		this.pokeDex = new PokeDex();
+		this.pokeDex.insertPokeDex();
 	}
-
-	public void printPokeDex() {
-
-	}
-
-	public boolean insertPokeDex() {
-		return true;
+	
+	// 사용자의 도감 출력
+	public void printMyPokeDex() {
+        this.pokeDex.printPokeDex();
+    }
+	
+	// 몬스터를 잡았을 때 잡은 몬스터의 이름을 기준으로 도감 정보 최신화
+	public void updateMyPokeDex(String name) {
+		this.pokeDex.updatePokeDex(name);
 	}
 }
