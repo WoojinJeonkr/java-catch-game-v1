abb772d7c7ba2c2d4acd856138dd4155435ee636
LJK0501 <whfh419@gmail.com>
2025-04-22 17:43:49 +0900

    style(map): 코드 정리 및 포맷 수정

--- catchGame/src/catchGame/user/User.java
+++ catchGame/src/catchGame/user/User.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class User {
 	public String userName;
 	public String location;
-	public MonsterBase[] pokeDex;
+	// public MonsterBase[] pokeDex;
 
 	Scanner scanner = new Scanner(System.in);
 
