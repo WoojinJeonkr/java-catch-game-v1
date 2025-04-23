6a749af1c229290a7e9fd234493e0fb2ad7133b1
Jeon Woojin <96942871+WoojinJeonkr@users.noreply.github.com>
2025-04-23 16:52:45 +0900

    feat: 유저의 몬스터 포획 및 포켓덱스 등록 기능 구현
    # 변경 사항
    
    - 유저 클래스에 PokeDex 연동
    - 포획한 몬스터를 위치 기반으로 출현시키고 등록
    - 사용자 상태 출력 및 맵 탐색 기능 추가

--- catchGame/src/catchGame/user/User.java
+++ catchGame/src/catchGame/user/User.java
@@ -3,12 +3,14 @@ package catchGame.user;
 import java.util.Scanner;
 
 import catchGame.map.MapExploring;
+import catchGame.monster.MonsterArrays;
+import catchGame.monster.MonsterBase;
 
 public class User {
 	public String userName; // 사용자 이름
 	public String location; // 사용자 위치
 	public PokeDex pokeDex; // 사용자 도감
-	public int mapCount; // 맵 반복 횟수
+	public MapExploring mapExploring; // 맵 정보
 
 	Scanner scanner = new Scanner(System.in);
 
@@ -18,6 +20,8 @@ public class User {
 		this.userName = scanner.nextLine();
 		this.pokeDex = new PokeDex();
 		this.pokeDex.insertPokeDex();
+		this.mapExploring = new MapExploring();
+		this.location = "집";
 	}
 
 	// 사용자의 도감 출력
@@ -25,23 +29,69 @@ public class User {
 		this.pokeDex.printPokeDex();
 	}
 
+	// 몬스터 포획 메소드
+	public void catchMonster() {
+		// 몬스터 배열 객체 선언
+		MonsterArrays monsterArrays = new MonsterArrays();
+		
+		boolean isCatch = false; // 포획 여부
+		String catchMonsterName = "";
+		
+		// 맵 정보 배열로 정의 ("하늘", "바다", "산", "랜덤", "취소")
+		String[] gameMap = this.mapExploring.map;
+		
+		// 현재 지역에 맞는 몬스터 배열 가져옴
+		if (this.location.equals(gameMap[0])) {
+			System.out.println("1. 조우");
+			MonsterBase skyMonster = monsterArrays.skyMonsters();
+			skyMonster.appearanceComment();
+			isCatch = skyMonster.catchMonster();
+			catchMonsterName = skyMonster.name;
+		} else if (this.location.equals(gameMap[1])) {
+			MonsterBase seaMonster = monsterArrays.seaMonsters();
+			seaMonster.appearanceComment();
+			isCatch = seaMonster.catchMonster();
+			catchMonsterName = seaMonster.name;
+		} else if (this.location.equals(gameMap[2])) {
+			MonsterBase earthMonster = monsterArrays.earthMonsters();
+			earthMonster.appearanceComment();
+			isCatch = earthMonster.catchMonster();
+			catchMonsterName = earthMonster.name;
+		} else if (this.location.equals(gameMap[3])) {
+			MonsterBase randomMonster = monsterArrays.universeMonsters();
+			randomMonster.appearanceComment();
+			isCatch = randomMonster.catchMonster();
+			catchMonsterName = randomMonster.name;
+		}
+		
+		// 포획했다면 도감에 등록
+		if (isCatch) {
+			System.out.println("2. 등록");
+			System.out.println("포획: " + catchMonsterName);
+			this.updateMyPokeDex(catchMonsterName);
+			System.out.println("3. 등록 완료");
+		}
+	}
+	
 	// 몬스터를 잡았을 때 잡은 몬스터의 이름을 기준으로 도감 정보 최신화
-	public void updateMyPokeDex(String name) {
-		this.pokeDex.updatePokeDex(name);
+	private void updateMyPokeDex(String monsterName) {
+		this.pokeDex.updatePokeDex(monsterName);
+	}
+	
+	// 도감 검색
+	public void searchTotalPokeDex() {
+		this.pokeDex.searchPokeDex();
 	}
 
 	// 맵 선택
 	public void selectMap() {
-		MapExploring map = new MapExploring();
-		map.mapInput(this.mapCount);
-		this.location = map.answerMap;
+		this.mapExploring.mapInput(this.mapExploring.mapIterationCount);
+		this.location = this.mapExploring.answerMap;
 	}
 
 	// 유저 정보 출력
 	public void printUserInfo() {
 		System.out.println("사용자명: " + this.userName);
 		System.out.println("사용자 위치: " + this.location);
-		System.out.println("사용자 도감");
-		this.pokeDex.printPokeDex();
 	}
 }
