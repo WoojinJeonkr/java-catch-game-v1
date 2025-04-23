ca7f357810e1ac3d789c9d12bd5c3aeaafdc01c6
LJK0501 <whfh419@gmail.com>
2025-04-23 17:34:14 +0900

    fix : manager 코드 수정

--- catchGame/src/catchGame/manage/GameManager.java
+++ catchGame/src/catchGame/manage/GameManager.java
@@ -7,50 +7,56 @@ import catchGame.user.User;
 public class GameManager {
 	private boolean isRunning; // 게임 상태
 	User user;
-	
 	Scanner scanner = new Scanner(System.in);
-	
+
 	// 객체 생성 시 user 객체 생성, 게임 상태 true로 설정
 	public GameManager() {
 		user = new User();
 		this.isRunning = true;
 	}
-	
+
 	// 게임 메뉴 출력 메소드
 	public void printGameMenu() {
 		System.out.println("\n=== 몬스터 잡기 게임 ===");
-	    System.out.println("1. 맵 선택하기");
-	    System.out.println("2. 몬스터 도감 보기");
-	    System.out.println("3. 내 정보 보기");
-	    System.out.println("4. 게임 종료");
-	    System.out.print("메뉴를 선택하세요 (1-4): ");
-	    int inputChoice = scanner.nextInt();
-	    
-	    switch (inputChoice) {
-	    	case 1:
-	    		this.user.selectMap();
-	    		System.out.println("\n>> 몬스터를 탐색 중입니다...");
-	    		// 탐색 로직 추가
-	    		break;
-	        case 2:
-	            System.out.println("\n>> 나의 몬스터 도감을 확인합니다.");
-	            // 도감 출력 메서드 호출
-	             user.printMyPokeDex();
-	            break;
-	        case 3:
-	            System.out.println("\n>> 내 정보를 확인합니다.");
-	            // 사용자 정보 출력
-	            user.printUserInfo();
-	            break;
-	        case 4:
-	            System.out.println("\n>> 게임을 종료합니다. 감사합니다!");
-	            this.isRunning = false;
-	            break;
-	        default:
-	            System.out.println("잘못된 입력입니다. 1 ~ 5 사이의 숫자를 입력하세요.");
-	    }
+		System.out.println("1. 맵 선택하기");
+		System.out.println("2. 몬스터 도감 보기");
+		System.out.println("3. 내 정보 보기");
+		System.out.println("4. 게임 종료");
+		System.out.print("메뉴를 선택하세요 (1-4): ");
+		int inputChoice = scanner.nextInt();
+
+		switch (inputChoice) {
+		case 1:
+			this.user.selectMap(); //
+			System.out.println();
+			System.out.println("--" + user.location + "맵에 소환되었습니다" + "--");
+			System.out.println("(..두리번...두리번..)");
+			System.out.println("\n>> 몬스터를 탐색 중입니다...");
+
+			user.catchMonster();
+			System.out.println("홈으로 이동하십겠습니까? (y/n)");
+			String answer = scanner.nextLine();
+			// 1. 배회하기(탐색하기) 2. 다른 맵 이동하기 3. 도감 보기 4. 홈으로가기
+			// 탐색 로직 추가
+			break;
+		case 2:
+			System.out.println("\n>> 나의 몬스터 도감을 확인합니다.");
+			// 도감 출력 메서드 호출
+			user.printMyPokeDex();
+			break;
+		case 3:
+			System.out.println("\n>> 내 정보를 확인합니다.");
+			// 사용자 정보 출력
+			user.printUserInfo();
+			break;
+		case 4:
+			System.out.println("\n>> 게임을 종료합니다. 감사합니다!");
+			this.isRunning = false;
+			break;
+		default:
+			System.out.println("잘못된 입력입니다. 1 ~ 5 사이의 숫자를 입력하세요.");
+		}
 	}
-	
 
 	// 게임 실행 메소드
 	public void runGame() {
--- catchGame/src/catchGame/map/MapExploring.java
+++ catchGame/src/catchGame/map/MapExploring.java
@@ -4,72 +4,70 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MapExploring {
-   public String[] map = { "하늘", "바다", "땅", "랜덤", "취소"};
-   Scanner scanner = new Scanner(System.in);
-   public String answerMap;
-   public int mapIterationCount; // 맵 선택하기 반복 횟수
-   public int answerGame; // SelectGame 번호 고르기
-   private int mapIndex = 1;
-   private int mapReturn;
+	public String[] map = { "하늘", "바다", "땅", "랜덤", "취소" };
+	Scanner scanner = new Scanner(System.in);
+	public String answerMap;
+	public int mapIterationCount; // 맵 선택하기 반복 횟수
+	private int mapIndex = 1;
+	private int mapReturn;
 
-   public void mapInput(int mapCount) {
-      this.mapIterationCount = mapCount;
-      
-      while (this.mapIndex == 1) {
-         System.out.println("\n+++ 몬스터 잡기 게임 +++");
-            System.out.println("🗺️ 맵을 선택하세요 (하늘☁️ |바다🌊 |땅🌴 |랜덤🎲 |취소⚠️)");
-            this.answerMap = scanner.nextLine();
-            
-            this.mapIndex = mapSelect(this.answerMap);
-            
-            if (this.mapIndex == 0) {
-                System.out.println("\n>> 맵 이동중입니다...");
-                this.mapIterationCount++;
-				break;				
-            }
-			if(this.mapIndex == 1){
+	public void mapInput(int mapCount) {
+		this.mapIterationCount = mapCount;
+
+		while (this.mapIndex == 1) {
+			System.out.println("\n+++ 몬스터 잡기 게임 +++");
+			System.out.println("🗺️ 맵을 선택하세요 (하늘☁️ |바다🌊 |땅🌴 |랜덤🎲 |취소⚠️)");
+			this.answerMap = scanner.nextLine();
+			this.mapIndex = mapSelect(this.answerMap);
+			if (this.mapIndex == 0) {
+				System.out.println("\n>> 맵 이동중입니다...");
+				this.mapIterationCount++;
+				break;
+			}
+			if (this.mapIndex == 1) {
 				break;
 			}
 
-      }
-   }
-   public int mapSelect(String answerMap) {
-      if (answerMap.equals("하늘")) {
-         System.out.println("하늘☁️ 맵 페이지로 이동합니다");
-         this.mapReturn = 0;
-      } else if (answerMap.equals("바다")) {
-         System.out.println("바다🌊 맵 페이지로 이동합니다");
-         this.mapReturn = 0;
-      } else if (answerMap.equals("산")) {
-         System.out.println("땅🌴 맵 페이지로 이동합니다");
-         this.mapReturn = 0;
-      } else if (answerMap.equals("랜덤")) {
-         System.out.println("랜덤🎲 맵 페이지로 이동합니다");
-         String mapProbabilityAnswer = mapProbability();
-         System.out.println(mapProbabilityAnswer + " 맵 페이지가 선택되었습니다");
-         this.mapReturn = 0;
-      } else if (answerMap.equals("취소")) {
-		System.out.println("홈 페이지로 이동합니다");
-		this.mapReturn = 1;
-	  }else {
-         System.out.println("잘못 입력하셨습니다");
-         this.mapReturn = 2;
-      }
-      return this.mapReturn;
-   }
+		}
+	}
 
-   // 히든 맵 나올 확률 0.718% => 718/100000로 확률 조작
-   public String mapProbability() {
-      Random random = new Random();
-      int index = random.nextInt(100000);
-      if (index < 33093) {
-         return "하늘";
-      } else if (index < 66187) {
-         return "바다";
-      } else if (index < 99281) {
-         return "땅";
-      } else {
-         return "👏👏👏 히든";
-      }
-   }
+	public int mapSelect(String answerMap) {
+		if (answerMap.equals("하늘")) {
+			System.out.println("하늘☁️ 맵 페이지로 이동합니다");
+			this.mapReturn = 0;
+		} else if (answerMap.equals("바다")) {
+			System.out.println("바다🌊 맵 페이지로 이동합니다");
+			this.mapReturn = 0;
+		} else if (answerMap.equals("산")) {
+			System.out.println("땅🌴 맵 페이지로 이동합니다");
+			this.mapReturn = 0;
+		} else if (answerMap.equals("랜덤")) {
+			System.out.println("랜덤🎲 맵 페이지로 이동합니다");
+			String mapProbabilityAnswer = mapProbability();
+			System.out.println(mapProbabilityAnswer + " 맵 페이지가 선택되었습니다");
+			this.mapReturn = 0;
+		} else if (answerMap.equals("취소")) {
+			System.out.println("홈 페이지로 이동합니다");
+			this.mapReturn = 1;
+		} else {
+			System.out.println("잘못 입력하셨습니다");
+			this.mapReturn = 2;
+		}
+		return this.mapReturn;
+	}
+
+	// 히든 맵 나올 확률 0.718% => 718/100000로 확률 조작
+	public String mapProbability() {
+		Random random = new Random();
+		int index = random.nextInt(100000);
+		if (index < 33093) {
+			return "하늘";
+		} else if (index < 66187) {
+			return "바다";
+		} else if (index < 99281) {
+			return "땅";
+		} else {
+			return "👏👏👏 히든";
+		}
+	}
 }
