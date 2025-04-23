10ba18b321de5589ae94b87bace7ec0c446af075
ImaginaryNumberi <pkm92@naver.com>
2025-04-23 16:29:03 +0900

    Monster0 클래스의 run,catch 메서드 수정
    1) 2025.04.23 09:00
    몬스터 관련 클래스
    MonsterArrays => 몬스터 등장 확률
    MonsterBase(0~10) => 몬스터 등장멘트, 도망칠 확률, 잡힐 확률
    
    2) 2025.04.23 10:00
    패키지 명 수정
    
    3) 2025.04.23 10:35
    MonsterBase의 ifCatch 파라미터 접근자 수정
    
    4) 2025.04.23 12:42
    패키지명 수정
    
    5) 2025.04.23 16:28
    Monster0 클래스의 run,catch 메서드 수정

--- catchGame/src/catchGame/monster/Monster0.java
+++ catchGame/src/catchGame/monster/Monster0.java
@@ -25,39 +25,19 @@ public class Monster0 extends MonsterBase {
 		} else {
 			System.out.println("야생의 아르세우스!는 커녕 아무것도 나오지 않았다.. 이동하자");
 		}
-		// 잡혔을 때
-		this.ifCatch = true;
 	}
-	
-//	// 도망칠 확률 "개별사건"
-//	// 10% => 1/10
-//	// 랜덤 값 1 나오면 도망 그외 숫자 안 도망침
-//	public boolean runMonster() {
-//		if(this.r.nextInt(10) + 1 == 1) {
-//			System.out.println("그 잉어킹이 10% 확률로 두려움을 느꼈다!");
-//			System.out.println("집에 가서 자랑해보자!");
-//			return false;
-//		}
-//			
-//		return true;
-//	}
-//		
-//	// 잡힐 확률 "개별사건"
-//	// 97% => 97/100
-//	// 반환 값 => 잡혔을 때:true | 아닐 때:false
-//	@Override
-//	public boolean catchMonster() {
-//	// 잡혔을 때 
-//		if(this.r.nextInt(100) + 1 > 3) {
-//			System.out.println(this.name + "가(이) 꽥 소리를 냈다!");
-//			System.out.println("97% 확률로 잡아냈다!");
-//			System.out.println("지나가던 꼬마도 잡을 수 있는 흔한 녀석이다 집에 가서 자랑해보자!");
-//			return true;
-//	}
-//			System.err.println(this.name + "의 모습이 보이지 않는다");
-//			System.err.println("3% 확률로 아뿔싸 놓치고 말았다");
-//			System.err.println("이것도 못잡는 거 보니 해당 유저는 사람새x끼가 아닌거 같다!");
-//			return false;
-//	}
+
+	public boolean runMonster() {
+		System.out.println("...");
+		return false;
+	}
+
+
+	@Override
+	public boolean catchMonster() {
+		System.out.println("...");
+		this.ifCatch = true;
+		return true;
+	}
 	
 }
