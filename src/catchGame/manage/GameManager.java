54f62102e95804ec260b98173d0eaf540525b1a6
WoojinJeonkr <wooeric1@naver.com>
2025-05-02 10:45:39 +0900

    fix: 잘못된 indent 수정

--- src/catchGame/manage/GameManager.java
+++ src/catchGame/manage/GameManager.java
@@ -18,8 +18,8 @@ import catchGame.user.User;
  * @author Woojinjeonkr
  */
 public class GameManager {
-	/** 게임 실행 상태 플래그 */
-	private boolean isRunning;
+    /** 게임 실행 상태 플래그 */
+    private boolean isRunning;
 	/** 사용자 입력을 처리하는 Scanner 객체 */
     private Scanner scanner;
     /** 이전 위치 정보 */
